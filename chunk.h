e82431a1feec1ab866ba7efeb8d0ff280f019d28
harrisjib216 <harrisjib216@gmail.com>
2023-02-25 17:29:03 -0500

    constants supported

--- chunk.h
+++ chunk.h
@@ -15,6 +15,7 @@ typedef struct
     int count;
     int capacity;
     uint8_t *code;
+    int *lines;
     ValueArray constants;
 } Chunk;
 
@@ -22,7 +23,7 @@ void initChunk(Chunk *chunk);
 
 void freeChunk(Chunk *chunk);
 
-void writeChunk(Chunk *chunk, uint8_t byte);
+void writeChunk(Chunk *chunk, uint8_t byte, int line);
 
 int addConstant(Chunk *chunk, Value value);
 
