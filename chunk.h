3c2294fd5a1a0807ecc16ca82d5bba4bdea0623d
harrisjib216 <harrisjib216@gmail.com>
2023-02-25 17:22:21 -0500

    constants

--- chunk.h
+++ chunk.h
@@ -2,9 +2,11 @@
 #define blue_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 typedef enum
 {
+    OP_CONSTANT,
     OP_RETURN,
 } OpCode;
 
@@ -13,6 +15,7 @@ typedef struct
     int count;
     int capacity;
     uint8_t *code;
+    ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk *chunk);
@@ -20,5 +23,7 @@ void initChunk(Chunk *chunk);
 void freeChunk(Chunk *chunk);
 
 void writeChunk(Chunk *chunk, uint8_t byte);
+
+int addConstant(Chunk *chunk, Value value);
 
 #endif
---
+++ value.h
@@ -0,0 +1,23 @@
+#ifndef blue_value_h
+#define blue_value_h
+
+#include "common.h"
+
+typedef double Value;
+
+typedef struct
+{
+    int capacity;
+    int count;
+    Value *values;
+} ValueArray;
+
+void initValueArray(ValueArray *array);
+
+void writeValueArray(ValueArray *array, Value value);
+
+void freeValueArray(ValueArray *array);
+
+void printValue(Value value);
+
+#endif
