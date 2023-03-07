22a1244734248e716aef636509925c283346b7bb
harrisjib216 <harrisjib216@gmail.com>
2023-03-07 12:53:54 -0500

    compiling

--- compiler.c
+++ compiler.c
@@ -1,13 +1,104 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
 
+typedef struct
+{
+    Token current;
+    Token previous;
+    bool hadError;
+    bool panicMode;
+} Parser;
+
+Parser parser;
+
+// print where the error occurred and its message
+static void errorAt(Token *token, const char *message)
+{
+    // ignore other errors
+    // todo: remove this so all errors can log
+    if (parser.panicMode)
+        return;
+
+    // for exceptions
+    parser.panicMode = true;
+
+    // print error line
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type == TOKEN_EOF)
+    {
+        fprintf(stderr, " at end");
+    }
+    else if (token->type == TOKEN_ERROR)
+    {
+        // todo: figure out how to fix this
+    }
+    else
+    {
+        // print token if possible
+        fprintf(stderr, " at '%.*s'", token->length, token->start);
+    }
+
+    // print our error message
+    fprintf(stderr, ": %s\n", message);
+
+    // inform we had an error
+    parser.hadError = true;
+}
+
+// tell the user of error token from scanner
+static void errorAtCurrent(const char *message)
+{
+    errorAt(&parser.current, message);
+}
+
+// "just found an error"
+static void error(const char *message)
+{
+    errorAt(&parser.previous, message);
+}
+
+// advance to get next token
+static void advance()
+{
+    parser.previous = parser.current;
+
+    for (;;)
+    {
+        parser.current = scanToken();
+
+        if (parser.current.type != TOKEN_ERROR)
+            break;
+
+        errorAtCurrent(parser.current.start);
+    }
+}
+
+// read token and validate expected type
+static void consume(TokenType type, const char *message)
+{
+    if (parser.current.type == type)
+    {
+        advance();
+        return;
+    }
+
+    errorAtCurrent(message);
+}
+
+// compilation was successful if no error appeared
 bool compile(const char *source, Chunk *chunk)
 {
-    // generate tokens from code
+    // make a scanner to generate tokens from code
     initScanner(source);
+
+    // initialize parser errors to false
+    parser.hadError = false;
+    parser.panicMode = false;
 
     // prime the pump
     advance();
@@ -17,4 +108,6 @@ bool compile(const char *source, Chunk *chunk)
 
     // make sure we reach eof
     consume(TOKEN_EOF, "Expected end of expression");
+
+    return !parser.hadError;
 }
